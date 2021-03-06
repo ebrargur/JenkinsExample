67ba32a0a7020011c05b5c09d5605a0bc222e00e
ebrargur <ebrarusa@gmail.com>
2021-03-06 12:30:16 -0500

    my fourth commit

--- src/Firstry.java
+++ src/Firstry.java
@@ -2,7 +2,7 @@ public class Firstry {
     public static void main(String[] args) throws InterruptedException {
 // write your code here
         for (int i = 0; i <4 ; i++) {
-            System.out.println("Hebfmmmhnllo batch");
+            System.out.println("Hebfmmmvbbhnllo batch");
             Thread.sleep(1000);
 
         }
