24920089f0829cd19f4e2032333241ee212c374b
ebrargur <ebrarusa@gmail.com>
2021-03-07 10:57:17 -0500

    my fourth commitr

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 // write your code here
         for (int i = 0; i <4 ; i++) {
-            System.out.println("Hebfrrrmmmrvbbhnllo batch");
+            ystem.out.println("Hebfrrrmmmrvbbhnllo batch");
             Thread.sleep(1000);
 
         }
